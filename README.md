5d40f7c1b985a406c78620e27073a9d376bd0221
Neon <neonfamxxi@gmail.com>
2023-11-15 15:40:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an assignment for Athenian's Data Structures and Algorithms course. See instructions on Canvas.
---
+++ src/test/java/MyTests.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTests {
+    @Test
+    public void testInsertFirst() {
+        assertTrue(false);
+    }
+}
