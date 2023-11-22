382c03cd902368ac1342befebeea6affa8b2d7fb
NeonD00m <neond00m.business@gmail.com>
2023-11-22 08:39:23 -0800

    done I think

---
+++ src/main/java/Trie.java
@@ -0,0 +1,131 @@
+package main.java;
+
+public class Trie {
+    TrieNode root;
+
+    // Each Trie starts with a root node
+    // All children are currently null
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a String into the Trie
+    public void add(String str) {
+        return;
+    }
+
+    // Searches the Trie for a String
+    public boolean contains(String str) {
+        return false;
+    }
+
+    // "Deletes" a String from the Trie
+    // In actuality, we will just unmark the String as a word
+    public void remove(String str) {
+        return;
+    }
+
+    // Calls the printRec method
+    public void print() {
+        printRec(this.root, "");
+    }
+
+    // Recursively prints out the Trie
+    // String str will keep track of the "word so far"
+    // Should print out any words
+    // Should call the printRec method on all 26 children
+    public void printRec(TrieNode current, String str) {
+        // YOUR CODE HERE
+        return;
+    }
+
+    public static void main(String[] args) {
+        // --------------------------
+        // Test 1: Insertion (add)
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 1: Insertion (add)");
+        System.out.println("Expected:");
+        System.out.println("hi\n" +
+                "his\n" +
+                "hiss\n" +
+                "history\n" +
+                "world\n");
+
+        System.out.println("\nGot:");
+
+        Trie test = new Trie();
+
+        test.add("hi");
+        test.add("world");
+        test.add("his");
+        test.add("history");
+        test.add("hiss");
+
+        test.print();
+        System.out.println();
+        // hi
+        // his
+        // hiss
+        // history
+        // world
+
+        // --------------------------
+        // Test 2: Deletion (remove)
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 2: Deletion (remove)");
+        System.out.println("Expected:");
+        System.out.println("hi\n" +
+                "hiss\n" +
+                "history\n" +
+                "world\n");
+
+        System.out.println("\nGot:");
+        test.remove("his");
+        test.print();
+        System.out.println();
+        // hi
+        // hiss
+        // history
+        // world
+
+        // --------------------------
+        // Test 3: Search (contains) and other tests
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 3: Search (contains) and other tests");
+        System.out.println("Expected:");
+        System.out.println("false\n" +
+                "true\n" +
+                "false\n" +
+                "false");
+
+        System.out.println("\nGot:");
+        Trie trie1 = new Trie();
+        System.out.println(trie1.contains("card"));
+        // false
+
+        trie1.add("card");
+        System.out.println(trie1.contains("card"));
+        // true
+        System.out.println(trie1.contains("car"));
+        // false
+
+        trie1.remove("card");
+        System.out.println(trie1.contains("card"));
+        // false
+    }
+}
+
+// Each TrieNode keeps track of if it is the end
+// of a word, as well as an array of 26 possible children
+class TrieNode {
+    TrieNode[] children;
+    boolean isWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isWord = false;
+    }
+}
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,12 +1,85 @@
 package test.java;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
+    private static boolean arrayEqualsUpTo(int lastIndexExclusive, String[] arr1, String[] arr2) {
+        for (int i = 0; i < lastIndexExclusive; i++) {
+            if (!arr1[i].equals(arr2[i])) {
+                System.out.println("Trie does not match expected results at index " + i +
+                        ". Got " + arr2[i] + " instead of " + arr1[i]);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void trieToArrayRec(TrieNode current, String str, String[] words, int[] i2) {
+        if (current.isWord) {
+            words[i2[0]++] = str;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (current.children[i] == null) {
+                continue;
+            }
+            trieToArrayRec(current.children[i], str + (char) (i + 'a'), words, i2);
+        }
+    }
+
+    private static void trieEquals(Trie test, String[] expected) {
+        String[] words = new String[10];
+        int[] i = new int[1];
+        trieToArrayRec(test.root, "", words, i);
+        assertTrue(arrayEqualsUpTo(expected.length, expected, words));
+    }
+
     @Test
-    public void testInsertFirst() {
-        assertTrue(false);
+    public void insertionAdd() {
+        // --------------------------
+        // Test 1: Insertion (add)
+        // --------------------------
+        Trie test = new Trie();
+
+        test.add("hi");
+        test.add("world");
+        test.add("his");
+        test.add("history");
+        test.add("hiss");
+
+        trieEquals(test, new String[] {"hi", "his", "hiss", "history", "world"});
+    }
+
+    @Test
+    public void deletionRemove() {
+        Trie test = new Trie();
+        test.add("hi");
+        test.add("world");
+        test.add("his");
+        test.add("history");
+        test.add("hiss");
+        // --------------------------
+        // Test 2: Deletion (remove)
+        // --------------------------
+        test.remove("his");
+
+        trieEquals(test, new String[] {"hi", "hiss", "history", "world"});
+    }
+
+    @Test
+    public void searchContains() {
+        // --------------------------
+        // Test 3: Search (contains) and other tests
+        // --------------------------
+        Trie trie1 = new Trie();
+        assertFalse(trie1.contains("card"));
+
+        trie1.add("card");
+        assertTrue(trie1.contains("card"));
+        assertFalse(trie1.contains("car"));
+
+        trie1.remove("card");
+        assertFalse(trie1.contains("card"));
     }
 }
